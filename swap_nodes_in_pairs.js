613339dc42d67eb841be748300efa8c6041cd1f9
Leyla <lisana5632@gmail.com>
2019-01-14 00:42:56 +0300

    changed swapPairs:values shouldn't be modified

--- swap_nodes_in_pairs.js
+++ swap_nodes_in_pairs.js
@@ -8,16 +8,18 @@
   Given 1->2->3->4, you should return the list as 2->1->4->3
 
 */
-var swapPairs = function(head) {
-    if(!head || !head.next) return head;
-    let ptr=head;
-    while(ptr.next) {
-        let cur_val=ptr.val;
-        let next_val=ptr.next.val;
-        ptr.val=next_val;
-        ptr.next.val=cur_val;
-        if(ptr.next.next) ptr=ptr.next.next;
-        else ptr=ptr.next;
+
+  var swapPairs = function(head) {
+    if (!head || !head.next) return head;
+    let prev=null,ptr1=head,ptr2=ptr1.next;
+    while (ptr2) {
+      if (!prev) head = ptr2;
+      else prev.next = ptr2;
+      ptr1.next = ptr2.next;
+      ptr2.next = ptr1;
+      prev = ptr1;
+      ptr1 = ptr1.next;
+      ptr2 = ptr1&&ptr1.next?ptr1.next:null;
     }
-   return head;
-};
+    return head;
+  }
