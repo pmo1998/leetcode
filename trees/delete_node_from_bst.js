8a281104c1704144c470388a20012ed3e9eaa812
Leyla <lisana5632@gmail.com>
2019-01-20 19:33:36 +0300

    Delete node from BST

---
+++ trees/delete_node_from_bst.js
@@ -0,0 +1,61 @@
+/*
+Given a root node reference of a BST and a key, delete the node with the given key in the BST.
+Return the root node reference (possibly updated) of the BST.
+Basically, the deletion can be divided into two stages:
+    Search for a node to remove.
+    If the node is found, delete the node.
+
+Note: Time complexity should be O(height of tree).
+Example:
+root = [5,3,6,2,4,null,7]
+key = 3
+
+    5
+   / \
+  3   6
+ / \   \
+2   4   7
+
+Given key to delete is 3. So we find the node with value 3 and delete it.
+One valid answer is [5,4,6,2,null,null,7], shown in the following BST.
+
+    5
+   / \
+  4   6
+ /     \
+2       7
+
+Another valid answer is [5,2,6,null,4,null,7].
+
+    5
+   / \
+  2   6
+   \   \
+    4   7
+*/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+ const getMin=(root)=> {
+   while(root&&root.left) root=root.left;
+   return root;
+}
+
+ var deleteNode = function(root,key) {
+     if(!root) return root;
+     if(key<root.val) root.left=deleteNode(root.left,key);
+     else if(key>root.val) root.right=deleteNode(root.right,key);
+     else {
+        if(!root.left) return root.right;
+        if(!root.right) return root.left;
+        let min=getMin(root.right);
+        root.val=min.val;
+        root.right=deleteNode(root.right,min.val);
+     }
+     return root;
+ };
