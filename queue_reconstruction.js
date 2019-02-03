94c3d49c2253c81016ca6d47a3c9b0be3a37c472
Leyla <lisana5632@gmail.com>
2019-02-03 18:23:33 +0300

    . Queue Reconstruction by Height

---
+++ queue_reconstruction.js
@@ -0,0 +1,26 @@
+/*
+Suppose you have a random list of people standing in a queue.
+Each person is described by a pair of integers (h, k), where h is the height of the person and k is the number of people in front of this person who have a height greater than or equal to h. Write an algorithm to reconstruct the queue.
+
+Note:
+The number of people is less than 1,100.
+Example
+Input:
+[[7,0], [4,4], [7,1], [5,0], [6,1], [5,2]]
+Output:
+[[5,0], [7,0], [5,2], [6,1], [4,4], [7,1]]
+*/
+
+const insert = (p,index, item) =>{
+  p.splice(index, 0, item);
+    return p;
+};
+
+var reconstructQueue = function(people) {
+    people.sort((a,b)=>a[0]!==b[0]?(b[0]-a[0]):(a[1]-b[1]));
+    let result=[];
+    for(let i=0;i<people.length;i++){
+        result=insert(result,people[i][1],people[i]);
+    }
+    return result;
+};
