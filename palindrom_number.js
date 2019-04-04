6e727b675a7cce258ed8f1958a921806cda60439
Leyla <lisana5632@gmail.com>
2019-04-04 23:48:03 +0300

    Valid parenthesis

---
+++ palindrom_number.js
@@ -0,0 +1,35 @@
+/*
+Determine whether an integer is a palindrome. An integer is a palindrome when it reads the same backward as forward.
+
+Example 1:
+
+Input: 121
+Output: true
+
+Example 2:
+
+Input: -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+
+Example 3:
+
+Input: 10
+Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+
+*/
+
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+const  isPalindrome = x => {
+  if (x < 0)  return false;
+  let newX = x, reverse = 0;
+  while (newX > 0) {
+    reverse = reverse * 10 + (newX % 10);
+    newX = Math.floor(newX / 10);
+  }
+  return reverse === x;
+};
